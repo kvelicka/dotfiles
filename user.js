1d02057dd9ad2f9850c1fc00cc140224b3ac1f44
Karl Velicka <karolis.velicka@gmail.com>
2020-02-27 09:16:04 +0000

    add firefox user.js

---
+++ user.js
@@ -0,0 +1,15 @@
+// Firefox user config file
+// Typically placed in ~/.mozilla/firefox/$USER_PROFILE/user.js
+
+// disable updating addon state from the Sync server - useful if you want to
+// disable something _only_ on this machine
+user_pref("services.sync.addons.ignoreUserEnabledChanges", true);
+
+// disable "Pocket" extension
+user_pref("extensions.pocket.enabled", false);
+
+// double click only selects the word from an URL, not the whole thing
+user_pref("browser.urlbar.doubleClickSelectsAll", true);
+
+// don't warn about accessing about:config
+user_pref("browser.aboutConfig.showWarning", false);
