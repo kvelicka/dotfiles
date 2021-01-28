3a29e9965603b55409ecf3d951c3d1129b0d4d87
Karl Velicka <karolis.velicka@gmail.com>
2021-01-28 18:04:27 +0000

    new soundcard picker with fzf

--- scripts/soundcard.sh
+++ scripts/soundcard.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 set -e errexit
 
-A2DJ_SINK=$(pactl list sinks| grep -e 'DJ' -B 2 | head -n 1 | cut -d'#' -f 2)
-echo $A2DJ_SINK
-pactl set-default-sink $A2DJ_SINK
+pactl list sinks | rg -e '^Sink ' -e 'Name: ' | fzf --tac | cut -d# -f2 | xargs --no-run-if-empty pactl set-default-sink
